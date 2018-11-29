e34c586d5b38803cff30db0f74b139440c50617d
graeme <grae22@gmail.com>
2018-11-29 20:08:06 +0000

    Missing from prev commit.

---
+++ Entities/ClimbSimulation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CRP.Entities
+{
+  internal class ClimbSimulation
+  {
+    private readonly List<GearCarrier> _climbers = new List<GearCarrier>();
+
+    public void AddClimber(in GearCarrier climber)
+    {
+      _climbers.Add(climber);
+    }
+  }
+}
